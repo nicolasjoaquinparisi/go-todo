e4a98f18589177478760aae65d104148ac4a488f
Nicolás Joaquín Parisi <78542476+NicolasJoaquinParisi@users.noreply.github.com>
2023-09-10 19:51:55 -0300

    feature: added enum task_status creation in database migration

--- database/migrate.go
+++ database/migrate.go
@@ -6,7 +6,9 @@ import (
 )
 
 func Migrate() {
-	err := DB.AutoMigrate(&models.User{})
+	DB.Exec(`CREATE TYPE enum_task_status AS ENUM ('ToDo', 'InProgress', 'Complete');`)
+
+	err := DB.AutoMigrate(&models.User{}, &models.Project{}, &models.Task{})
 
 	if err != nil {
 		fmt.Print("Failed to generate migration")
